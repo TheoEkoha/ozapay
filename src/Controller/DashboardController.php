25b9ce65abb1b88d5cd38bd0c47e9da9d612802c
TheoEkoha <olivieri.theo@gmail.com>
2025-02-22 04:20:02 +0100

    fix dashboard: 4

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,22 +8,28 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
-
+use App\Entity\User\User; // Assurez-vous que le bon namespace est utilisé pour votre entité User
 
 class DashboardController extends AbstractController
 {
-    public function __construct(private readonly TranslatorInterface $translator, private LoggerInterface $logger, private Security $security)
-    {
+    public function __construct(
+        private readonly TranslatorInterface $translator,
+        private LoggerInterface $logger,
+        private Security $security
+    ) {
     }
 
     #[Route('/admin', name: 'dashboard')]
     public function index(): Response
     {
-
         $user = $this->security->getUser(); // Récupère l'utilisateur connecté
 
-        if ($user) {
+        if ($user instanceof User) {
             $this->logger->info('Utilisateur connecté : ' . $user->getUsername());
+            // Mettez à jour la locale de l'application ici si nécessaire
+            // Utilisez la locale de l'utilisateur pour ajuster le contenu affiché
+            // Vous pourriez vouloir changer la locale ici si nécessaire
+            // $this->get('translator')->setLocale($user->getLocal());
         } else {
             $this->logger->warning('Aucun utilisateur connecté.');
         }
