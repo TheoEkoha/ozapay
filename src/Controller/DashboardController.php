10673bf47cc18a354e84375acd4d2501979568cf
TheoEkoha <olivieri.theo@gmail.com>
2025-02-22 04:17:49 +0100

    fix dashboard: 3

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -7,24 +7,26 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 
 
 class DashboardController extends AbstractController
 {
-    public function __construct(private readonly TranslatorInterface $translator, private LoggerInterface $logger)
+    public function __construct(private readonly TranslatorInterface $translator, private LoggerInterface $logger, private Security $security)
     {
     }
 
     #[Route('/admin', name: 'dashboard')]
     public function index(): Response
     {
-        // $user = $this->security->getUser(); // Récupère l'utilisateur connecté
 
-        // if ($user) {
-        //     $this->logger->info('Utilisateur connecté : ' . $user->getUsername());
-        // } else {
-        //     $this->logger->warning('Aucun utilisateur connecté.');
-        // }
+        $user = $this->security->getUser(); // Récupère l'utilisateur connecté
+
+        if ($user) {
+            $this->logger->info('Utilisateur connecté : ' . $user->getUsername());
+        } else {
+            $this->logger->warning('Aucun utilisateur connecté.');
+        }
 
         return $this->render('pages/dashboard/dashboard-sales.html.twig', [
             'page_title' => $this->translator->trans('Dashboard'),
