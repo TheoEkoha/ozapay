f11682369ec17bcc07380a1b56fe83c98d8dcf44
TheoEkoha <olivieri.theo@gmail.com>
2025-02-22 04:12:01 +0100

    new logger security

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
-use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Security; // Updated namespace
 use Psr\Log\LoggerInterface;
 
 
