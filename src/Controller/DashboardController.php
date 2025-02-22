20fd0a86f08d14e49b262130cfd84a51bc781f29
TheoEkoha <olivieri.theo@gmail.com>
2025-02-22 03:51:16 +0100

    fix: 36 - PUT /user

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Psr\Log\LoggerInterface;
 
 class DashboardController extends AbstractController
 {
